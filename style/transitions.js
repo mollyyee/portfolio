9b09b2516b36119e1537250da8c84f99934d26f0
Molly Yee <molly.yee@berkeley.edu>
2019-08-13 08:56:30 -0700

    Add files via upload

---
+++ style/transitions.js
@@ -0,0 +1,19 @@
+// $("#desktopNav a").on("click", function (event) {
+//     event.preventDefault()
+
+//     const href = $(this).attr("href")
+
+
+
+//     // $.ajax({
+//     //     url: 
+//     // })
+
+// })
+$(document).ready(function(){
+
+    $(function() {
+        $(".fadein").delay(800).fadeIn();
+    });
+
+})
