2ae4430247f123706abb7f962842c2683c6b2bd4
Molly Yee <molly.yee@berkeley.edu>
2020-02-09 20:53:06 -0800

    Add files via upload
    new fonts and animations and styling

--- style/transitions.js
+++ style/transitions.js
@@ -1,19 +1,38 @@
-// $("#desktopNav a").on("click", function (event) {
-//     event.preventDefault()
-
-//     const href = $(this).attr("href")
-
-
-
-//     // $.ajax({
-//     //     url: 
-//     // })
-
-// })
 $(document).ready(function(){
 
     $(function() {
         $(".fadein").delay(800).fadeIn();
     });
 
+    $('.casecard').hover(
+        function () 
+            {
+            $("#line").css(
+                {'border-bottom': 'solid 6px #ffffff'});
+            },
+        function () 
+            {
+            $("#line").css(
+                {'border-bottom': 'solid 6px #000000'});
+            },
+        );
+
 })
+
+
+// $("#caseStudyImg").animate({transform: 'scale(2)'}, 100);},
+
+// $(document).ready(function(){
+
+//     $(function() {
+//         $(".fadein").delay(800).fadeIn();
+//     });
+
+//     $('.casecard').hover(
+//         function () {
+//             $("#line").hide();
+//             $("#caseStudyImg").css({'padding-top': '24px'});},
+//         function () {
+//         $("#line").show(); 
+//         $("#caseStudyImg").css({'padding-top': '0px'});}),
+// })
