249f492b5e3c16293a657dac3a27fd09e52ef3d1
Molly Yee <molly.yee@berkeley.edu>
2020-02-17 15:57:14 -0800

    Add files via upload

---
+++ transitions.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+    $(function() {
+        $(".fadein").delay(800).fadeIn();
+    });
+    
+
+    $("#menu").click(function(){
+        $(".mobile-menu").fadeIn(200);
+      });
+
+      $(".dismiss").click(function(){
+        $(".mobile-menu").fadeOut(200);
+      });
+
+    $('.casecard').hover(
+        function () 
+            {
+            $("#line").css(
+                {'border-bottom': 'solid 6px #ffffff'});
+            },
+        function () 
+            {
+            $("#line").css(
+                {'border-bottom': 'solid 6px #000000'});
+            },
+        );
+
+})
+
+
+// $("#caseStudyImg").animate({transform: 'scale(2)'}, 100);},
+
+// $(document).ready(function(){
+
+//     $(function() {
+//         $(".fadein").delay(800).fadeIn();
+//     });
+
+//     $('.casecard').hover(
+//         function () {
+//             $("#line").hide();
+//             $("#caseStudyImg").css({'padding-top': '24px'});},
+//         function () {
+//         $("#line").show(); 
+//         $("#caseStudyImg").css({'padding-top': '0px'});}),
+// })
